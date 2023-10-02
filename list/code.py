f59a16830c3f66cda623d3ceb4879f40f720ef38
vinodsarwade <vinodsarwade7@gmail.com>
2023-10-02 19:37:12 +0530

    vinod

---
+++ list/code.py
@@ -0,0 +1,30 @@
+
+#list is a ordered collection of data items
+#list items seperated by quamas and enclosed with square brackets. 
+#list are changable / mutable
+# l = [12,20,30,44,52]
+# print(l)
+# print(type(l))
+# print(l[0])
+# print(l[1])
+
+l = [20,21,67,"vinod",]  # we can add any data type in list
+print(l[-3])    #negative indexing .to find negative index first find length of list then minus the index from len(list)
+print(len(l))     # which will give you a easest way to find negative index
+print(l[len(l)-3])   # this 4 lines of code has same output .
+print(l[4-3])
+
+
+
+#find elements in list
+if 20 in l:
+    print("yes")
+else:
+    print("no")
+
+#find charactor in string which is in list
+if "no" in "vinod":
+    print("yes")
+else:
+    print("no")
+
---
+++ list/list_comprehension.py
@@ -0,0 +1,7 @@
+#list comprehension means on the fly generate a new list
+
+lst =[i*i for i in range(5)]    # it will create a list up to 5 while iterating each element is multiply by i
+print(lst)
+
+lst =[i*i for i in range(5) if i%2==0]  #newly created list  which has elements divisible by 2 and its modulus is 0
+print(lst)                              #those elements is only added in list
