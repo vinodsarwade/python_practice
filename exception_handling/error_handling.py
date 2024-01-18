6f79537757fe8697283a4665f4fe144b1382f294
vinodsarwade <vinodsarwade7@gmail.com>
2024-01-18 23:23:48 +0530

    by vinod

---
+++ exception_handling/error_handling.py
@@ -0,0 +1,28 @@
+a = input("enetr the number: ")
+print(f"Multiplication  table of {a} is :")
+  
+#here we use try and except keyword to handle exception. if user gives int input then program exicute succesfully.
+#but user gives other than int like string boolean then program gives error. so that error handling is done by using try and except.
+#if try block gives error then it will print exception error and again  below remaining program will exicute.
+try:    
+    for i in range(1, 11):
+        print(f"{int(a)} X {i} = {int(a)*i}")
+except Exception as e: #or we can write only except: 
+    print(e)
+
+print("below are some imp lines of code")
+
+
+## Ex:
+try:
+    num = int(input("enter an integer input :"))
+    a = [5,6,3]    # here we have only 3 index list which is 0,1,2
+    print(a[num]) #it will print the element of index [num] which is given by user 
+                   #if index element is present then it will give element present in index. if index is not present in list then throw error.
+except ValueError:
+    print("entered nuber is not an integer")
+
+except:
+    print("index error")
+
+        
---
+++ exception_handling/finally.py
@@ -0,0 +1,27 @@
+#finally keyword is always exicuted weather the try block exicuted or except block exicuted.
+#code succefull run or it is throwing error finally block always exicuted.
+
+# try:
+#     lst = [3,5,7,9]
+#     num = int(input("enter an integer number: "))
+#     print(lst[num])
+# except:
+#     print("some error occured")
+
+# finally:
+#     print("am always exicuted. ")
+
+
+
+#using function
+def fun1():
+    try:
+        lst = [5,6,7,8]
+        num = int (input("enter an integer input : "))
+        print(lst[num])
+    except:
+        print("some error occured !!")
+    
+    finally:
+        print("am always exicuted:!")
+fun1()
