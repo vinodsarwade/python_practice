d1f06b3ec4e8ffde5b878a4ba751ace04555b7a9
vinodsarwade <vinodsarwade7@gmail.com>
2024-01-22 22:51:48 +0530

    vinod

---
+++ exercise4.py
@@ -0,0 +1,5 @@
+#secret code encoding and decoding
+#if string has 3 or less than 3 word then reverse that string 
+#if string has more than 3 words then append first letter to end of string ex: vinod -> inodv
+#after appending it add 3 random char at start and end of string
+# and last your program should ask decode also,if u want to decode.
