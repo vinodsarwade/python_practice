0723d9c4822a42899484a00ed8ed6cc150508de6
vinodsarwade <vinodsarwade7@gmail.com>
2024-01-31 22:34:37 +0530

    vinod

---
+++ main_fun/file.py
@@ -0,0 +1,6 @@
+# here we import vinod file and calling welcome  function which is in vinod.py
+#if i run this print statement will excecute twise
+#two avoid this need to use main function in pyhton   if__name__ =="" __main__"
+import vinod
+
+vinod.welcome()
---
+++ main_fun/vinod.py
@@ -0,0 +1,8 @@
+#code will start from main function only.
+#if i run this file print statement will exicute once.
+def welcome():
+    print("you are welcome")
+
+
+if __name__== "__main__": #by using this fun we can avoid twise printing.
+    welcome()
