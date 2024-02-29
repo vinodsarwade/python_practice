1fa0c4cedc2614bc8f95d794382e68b75749e22a
vinodsarwade <vinodsarwade7@gmail.com>
2024-02-29 22:13:34 +0530

    vinod

--- OS_Module/oslist.py
+++ OS_Module/oslist.py
@@ -6,4 +6,4 @@ for folder in folders: # print all the folders in column
     print(folder)
 
 for folder in folders:
-    print(os.listdir(f"data/{folder}")) # i ahve made another file in tutorial 10 ,it will display that file also 
+    print(os.listdir(f"data/{folder}")) # i have made another file in tutorial 10 ,it will display that file also 
--- exercise4.py
+++ exercise4.py
@@ -2,6 +2,30 @@
 #if string has 3 or less than 3 word then reverse that string 
 #if string has more than 3 words then append first letter to end of string ex: vinod -> inodv
 #after appending it add 3 random char at start and end of string
-# and last your program should ask decode also,if u want to decode.
+# and last your program should ask decode also,if u want to decode
 
-original_string = " "
+st = input("enter a message: ")
+words = st.split(" ")
+coding = False
+
+if(coding):
+    newwords = []
+    for word in words :
+        if(len(words)>=3):
+            r1= "dsf"
+            r2 = "gfv"
+            stnew = r1+word[1:]+word[0]+r2
+            newwords.append(stnew)
+        else:
+            newwords.append(word[:-1])
+    print(" ".join(newwords))
+else:
+    newwords= []
+    for word in words:
+        if (len(word>=3)):
+            stnew = word[3:-3]
+            stnew = stnew[-1]+stnew[:-1]
+            newwords.append(stnew)
+        else:
+            newwords.append(word[:-1])
+    print(" ".join(newwords))
