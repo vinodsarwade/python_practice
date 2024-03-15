4a3de118c209249f66a5fe8198c2b62312e37d0a
vinodsarwade <vinodsarwade7@gmail.com>
2024-03-15 14:09:03 +0530

    vinod

---
+++ OOPS/theory.py
@@ -0,0 +1,15 @@
+"""
+the basic idea of object oriented programming in python is to use classes and objects to represents 
+real world concept and entities.. 
+a class is a blueprint of templats for creating a object.
+it defines a properties and and methods that an object of a class will have.
+properties are a data or state of an object and methods are the action and behaviour that an object can perform.
+
+
+an object is a instance of a class and it contains it's own data and methods.
+
+ex:you could create a class called "person" and that has properties such as name and age and methods are speak() and walk().
+each instance of a person class would be unique object with it's own name and age,but they all have same methods to speak and walk.
+
+
+"""
