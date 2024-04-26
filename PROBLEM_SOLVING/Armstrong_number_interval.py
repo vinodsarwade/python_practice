502488fbe3988774a672e1cede019da762452457
vinodsarwade <vinodsarwade7@gmail.com>
2024-04-26 11:46:19 +0530

    vinod

---
+++ PROBLEM_SOLVING/Armstrong_number_interval.py
@@ -0,0 +1,14 @@
+lower = int(input("enter a lower limit number: "))
+upper = int(input("enter a upper limit number: "))
+
+for num  in range(lower, upper):
+    order = len(str(num)) 
+    sum = 0
+    temp = num
+    while temp > 0:
+        n = temp % 10 
+        cube = n**order
+        sum = sum + cube 
+        temp = temp // 10  
+    if sum == num:
+        print(num)
